94257759fb83813ac5550597b810d81b00944e7d
sgudiduri <siddu.gemeni@gmail.com>
2016-04-10 00:42:05 -0500

    Added files via upload

---
+++ FileSystems/DAL/DirectoryContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using FileSystems.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace FileSystems.DAL
+{
+    public class DirectoryContext : DbContext
+    {
+        public DbSet<Directory> Directories { get; set; }
+        public DbSet<File> Files { get; set; }
+
+        public DirectoryContext()
+            : base("DirectoryContext")
+        {
+            Database.SetInitializer<DirectoryContext>(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
